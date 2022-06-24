d619f3c163f294d56904e169739649c8fbf2e3ef
Alexander Gozali <alexandergozali0@gmail.com>
2022-06-24 15:10:47 +0700

    Feat: Login & Register Controller

---
+++ controllers/authController.js
@@ -0,0 +1,75 @@
+const { User } = require('../models');
+const { hashPassword, verifyPassword } = require('../helpers/passwordHandler');
+const { generateToken, verifyToken } = require('../helpers/tokenHandler');
+
+class AuthController {
+    static register = async (req, res) => {
+        try {
+            const { username, email, password, confirm_password, role } = req.body;
+    
+            const isUsernameExist = await User.findOne({ where: {username} });
+            if (isUsernameExist) return res.status(409).json({ message: 'Username is not available' });
+    
+            const isEmailExist = await User.findOne({ where: {email} });
+            if (isEmailExist) return res.status(409).json({ message: 'Email is not available' });
+    
+            if (password !== confirm_password) {
+                res.status(406).json({ message: 'Passwords do not match' });
+                return false;
+            } else {
+                const userPayload = {
+                    username: username,
+                    email: email,
+                    password: hashPassword(password),
+                    role: role
+                }
+    
+                const createUser = await User.create(userPayload);
+    
+                // Cookies
+                const access_token = await generateToken({
+                    id: createUser.id,
+                    username: createUser.username,
+                    role: createUser.role
+                })
+    
+                res.cookie('access_token', access_token, {
+                    httpOnly: true
+                })
+    
+                return res.status(201).json({ message: `User is Created, ${access_token}` });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static login = async (req, res) => {
+        try {
+            const { username, password } = req.body;
+
+            const loginUser = await User.findOne({ where: {username} });
+            if (!loginUser) return res.status(404).json({ message: 'User is not found' });
+            
+            const isPasswordMatch = verifyPassword(password, loginUser.password);
+            if (!isPasswordMatch) return res.status(409).json({ message: 'Password is wrong' });
+
+            // Cookie
+            const access_token = await generateToken({
+                id: loginUser.id,
+                username: loginUser.username,
+                role: loginUser.role
+            })
+
+            res.cookie('access_token', access_token, {
+                httpOnly: true
+            })
+
+            return res.status(200).json({ message: `Welcome, ${loginUser.username}, ${access_token}` });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+module.exports = AuthController;
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,13 +1,14 @@
 const v1 = require('express').Router();
 const userRouter = require('./user');
 const todoRouter = require('./todo');
+const AuthController = require('../../controllers/authController');
 
 v1.get('/', (req, res) => {
     res.json('hello from /v1')
 });
 
-v1.post('/register');
-v1.post('/login');
+v1.post('/register', AuthController.register);
+v1.post('/login', AuthController.login);
 
 v1.use('/todo', todoRouter);
 v1.use('/user', userRouter);
