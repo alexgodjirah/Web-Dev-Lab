3f4a832bda7bd77755acd410b1a5c35d7e287a61
Alexander Gozali <alexandergozali0@gmail.com>
2022-06-25 10:19:08 +0700

    Feat: JOI Validation

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,12 +3,14 @@ const userRouter = require('./user');
 const todoRouter = require('./todo');
 const AuthController = require('../../controllers/authController');
 const authentication = require('../../middlewares/authentication');
+const validatorHandler = require('../../middlewares/validation.joi');
+const { RegisterSchema } = require('../../validations');
 
 v1.get('/', (req, res) => {
     res.json('hello from /v1')
 });
 
-v1.post('/register', AuthController.register);
+v1.post('/register', validatorHandler(RegisterSchema, 'body'), AuthController.register);
 v1.post('/login', AuthController.login);
 
 v1.use(authentication);
