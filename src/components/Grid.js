c2cdab8e2ccea48c9823c253d1dac59a384e6415
Alexander Gozali <alexandergozali0@gmail.com>
2022-10-30 19:43:52 +0700

    Feat: Advance Grid and Row

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,11 @@ export default function Grid({ currentGuess, guesses, turn }) {
     return (
         <div>
             {guesses.map((guess, idx) => {
+                // To put ever letter inside the boxes by passing the data of current guess to the Row component (using turn and index to see if we input the same thing).
+                if (turn === idx) {
+                    console.log(turn);
+                    return <Row key={idx} currentGuess={currentGuess} />
+                }
                 return <Row key={idx} guess={guess} />
             })}
         </div>
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../index.css';
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
     // This will show the letter of each guess (which are passed from Grid.js)
     if (guess) {
         return (
@@ -13,7 +13,31 @@ export default function Row({ guess }) {
         )
     }
 
-    // If there are no guess, this will return.
+    // This will show each letter we type inside the boxes.
+    if (currentGuess) {
+        // Since currentGuess parameter is a string, we need to make it as an array -- using split method -- of letter. 
+        let letters = currentGuess.split('');
+
+        return (
+            <div className='row current'>
+                {/* Map through each letter provided by letter variable. */}
+                {letters.map((letter, idx) => (
+                    <div key={idx} className='filled' >
+                        {letter}
+                    </div>
+                ))}
+
+                {/* This array is used to make our box still visible when we type our answer */}
+                {
+                    [...Array(5 - letters.length)].map((_, idx) => (
+                        <div key={idx}></div>
+                    ))
+                }
+            </div>    
+        )
+    }
+
+    // If there are no guess and no currentGuess, this will return.
     return (
         <div className='row'>
             <div></div>
