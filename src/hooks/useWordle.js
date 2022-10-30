cdbddcba94f8f930249f13b46f38281a2b36c159
Alexander Gozali <alexandergozali0@gmail.com>
2022-10-30 14:29:21 +0700

    Feat: AddNewGuess() in useWordleHook()

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 const useWordle = (solution) => {
     const [turn, setTurn] = useState(0); // To contain how many turn it takes to answer; up by one each time the user take a guess. 6 out of 6 and the game is finished. 
     const [currentGuess, setCurrentGuess] = useState(''); // To contain the current guess. Track what the user is currently typing. Updated each time handleKeyUp() function is called. 
-    const [guesses, setGuesses] = useState([]); // Each guess is an array. Updated each time new guesses added and formatted by formatGuess() function. 
+    const [guesses, setGuesses] = useState([...Array(6)]); // Each guess is an array. Updated each time new guesses added and formatted by formatGuess() function. 
     const [history, setHistory] = useState([]); // Each guess is a string. More of less the same with guesses state, but using different data type: string. Used to make sure the user doesn't type the same guess.
     const [isCorrect, setIsCorrect] = useState(false); // Only change to true if the user wins the game. Used to call the congratulation modal.
 
-    // Format a guess nto an array of letter objects
+    // Format a guess into an array of letter objects
     // e.g. [{ key: 'a', color: 'yellow' }]
     const formatGuess = () => {
         let solutionArray = [...solution]; // spread the solution so we can check them one by one
@@ -38,8 +38,30 @@ const useWordle = (solution) => {
     // Add a new guess to the guesses state
     // Update the isCorrect state if the guess is correct
     // Add one to the turn state
-    const addNewGuess = () => {
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true);
+        }
 
+        // Add new guess to the guesses state
+        setGuesses((prevGuess) => {
+            let newGuesses = [...prevGuess];
+            newGuesses[turn] = formattedGuess;
+            return newGuesses;
+        })
+
+        // Add new guess to the history state
+        setHistory(prevHistory => {
+            return [...prevHistory, currentGuess]
+        });
+
+        // Add turn
+        setTurn(prevTurn => {
+            return prevTurn + 1;
+        });
+
+        // Clear currentGuess
+        setCurrentGuess('');
     }
 
     // Handle keyup event & track current guess
@@ -66,7 +88,7 @@ const useWordle = (solution) => {
             }
             
             const formatted = formatGuess();
-            console.log(formatted);
+            addNewGuess(formatted);
         }
         // Delete the last guess
         if (key === 'Backspace') {
